1abe0fbfc47620955d1ed758b6eb53c2491481c1
k0502s <k0502s@naver.com>
2021-01-22 17:52:03 +0900

    react and express login codeing..

--- bolier-plate/index.js
+++ bolier-plate/index.js
@@ -4,13 +4,15 @@ const port = 4000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 const config = require('./config/key');
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
-const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
@@ -26,13 +28,43 @@ app.post('/register', (req, res) => {
   const user = new User(req.body)
   //.save는 mongoDB의 메소드
   // 접속에 성공(status(200))하면 json을 이용하여 success: true 뜨게 함
-  user.save((err, userInfo) => {
+  user.save((err, user) => {
     if (err) return res.json({success: false, err})
     return res.status(200).json({
       success: true 
     })
   })
 })
+
+app.post('/login', (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+      return res.json({loginSuccess: false, message: "비밀번호 틀렸습니다."})
+      //비밀번호 맞다면 토큰을 생성하기
+      user.generateToken( (err, user) => {
+        if(err) return res.status(400).send(err);
+
+        //토큰을 저장한다. 어디에? 쿠키, 로컬 스토리지 등
+      res.cookie("x_auth", user.token)
+      .status(200)
+      .json({loginSuccess: true, userId: user._id})
+
+
+      })
+    })
+    })
+  })
+  
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- bolier-plate/models/User.js
+++ bolier-plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var jwt = require('jsonwebtoken');
 
 
 const userSchema = mongoose.Schema({
@@ -32,6 +35,49 @@ tokenExp: {
 }
 
 })
+
+//pre는 몽고DB의 메소드로 레지스터에서 정보를 저장
+//전에 콜백함수를 실행 시킨다는 의미다.
+userSchema.pre('save', function(next){
+  
+    //여기서 비밀번호 암호화 한다.
+
+    var user = this;
+    //.isModified메소드를 이용하여 패스워드가 변환될때만
+    //실행되게 if문 걸어줌
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+    else{
+        next()
+    }
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainPassword 1234567  암호화 번호 $2b$10$AS1tgbgoGFXe8BZGys/5RuEwxnEKsvIBu73SJbnCYdyvu58K.erhS
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+      if(err) return cb(err);
+      cb(null, isMatch)
+  })
+}
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    //jsonwebToken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+  user.token = token
+  user.save(function(err, user){
+      if(err) return cb(err);
+      cb(null, user)
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
